306be3ac3f43957e9b3db7599e11c3e07fa9bd52
Isomethyl-ionone <43140480+Isomethyl-ionone@users.noreply.github.com>
2018-11-21 00:03:27 +0200

    Update FastCollinearPoints.java

--- collinear/FastCollinearPoints.java
+++ collinear/FastCollinearPoints.java
@@ -11,45 +11,48 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class FastCollinearPoints {
-    private ArrayList<LineSegment> segmentsList = new ArrayList<>();
+    private final ArrayList<LineSegment> segmentsList = new ArrayList<>();
 
     public FastCollinearPoints(
             Point[] points) {    // finds all line segments containing 4 or more points
-        validation(points);
+
+        // validation
+        if (points == null) throw new IllegalArgumentException("null points");
+        for (int i = 0; i < points.length; i++) {
+            if (points[i] == null)
+                throw new IllegalArgumentException();
+            for (int j = 0; j < i; j++) {
+                if (points[j].compareTo(points[i]) == 0)
+                    throw new IllegalArgumentException();
+            }
+        }
+
+
         int n = points.length;
-        Point[] clonepoints = points.clone();
+        Point[] clonepoints = points.clone();                // don't mutate input array
         for (int i = 0; i < n; i++) {
-            // for (Point p : clonepoints) {
-            //     StdOut.println("clonepoints " + p);
-            // }
             Arrays.sort(clonepoints);
-            // for (Point p : clonepoints) {
-            //     StdOut.println("clonepoints sorted " + p);
-            // }
-            Arrays.sort(clonepoints, clonepoints[i].slopeOrder());
-            // for (Point p : clonepoints) {
-            //     StdOut.println("clonepoints sorted slope " + p);
-            // }
-            for (int j = i + 1; j < n - 2; j++) {
-                // System.out.println("i " + i);
-                // System.out.println("j " + j);
-                if (clonepoints[i].slopeTo(clonepoints[j]) == clonepoints[j]
+            Point origin
+                    = clonepoints[i];                  // need to store origin point after natural order sort and before slope sorting
+            Arrays.sort(clonepoints, origin.slopeOrder());  // stable sorting
+
+            for (int j = 1; j < n - 2; j++) {
+
+                if (origin.slopeTo(clonepoints[j]) == clonepoints[j]
                         .slopeTo(clonepoints[j + 1])
                         && clonepoints[j].slopeTo(clonepoints[j + 1]) == clonepoints[j + 1]
-                        .slopeTo(clonepoints[j + 2])) {
-                    Point[] gotpoints = new Point[4];
-                    gotpoints[0] = clonepoints[i];
+                        .slopeTo(clonepoints[j + 2]) && origin.compareTo(clonepoints[j]) < 0) {
+                    
+                    Point[] gotpoints = new Point[4];           // store all 4 collinear points
+                    gotpoints[0] = origin;
                     gotpoints[1] = clonepoints[j];
                     gotpoints[2] = clonepoints[j + 1];
                     gotpoints[3] = clonepoints[j + 2];
-                    Arrays.sort(gotpoints);
-                    // for (Point p : gotpoints) {
-                    //     StdOut.println("gotpoints " + p);
-                    // }
+                    Arrays.sort(
+                            gotpoints);                 // no problem with determining point end and first points with this approach
+
                     segmentsList.add(new LineSegment(gotpoints[0], gotpoints[3]));
-                    // for (LineSegment s : segmentsList) {
-                    //     StdOut.println("segmentsList " + s);
-                    // }
+
                 }
             }
         }
@@ -66,31 +69,21 @@ public class FastCollinearPoints {
         return lineSegments;
     }
 
-    private void validation(Point[] points) {
-        if (points == null) throw new IllegalArgumentException("null points");
-        for (int i = 0; i < points.length - 1; i++) {
-            if (points[i] == null) throw new IllegalArgumentException("null points");
-            for (int j = i + 1; j < points.length; j++) {
-                if (points[i].compareTo(points[j]) == 0)
-                    throw new IllegalArgumentException("null points");
-            }
-        }
-    }
 
     public static void main(String[] args) {
-        // read the n points from a file
-        In in = new In(args[0]);
-        int n = in.readInt();
-        Point[] points = new Point[n];
-        for (int i = 0; i < n; i++) {
-            int x = in.readInt();
-            int y = in.readInt();
-            points[i] = new Point(x, y);
-        }
-        FastCollinearPoints collinear = new FastCollinearPoints(points);
-        for (LineSegment lineSegments : collinear.segments()) {
-            StdOut.println("main " + lineSegments);
-        }
-        
+        // // read the n points from a file
+        // In in = new In(args[0]);
+        // int n = in.readInt();
+        // Point[] points = new Point[n];
+        // for (int i = 0; i < n; i++) {
+        //     int x = in.readInt();
+        //     int y = in.readInt();
+        //     points[i] = new Point(x, y);
+        // }
+        // FastCollinearPoints collinear = new FastCollinearPoints(points);
+        // for (LineSegment lineSegments : collinear.segments()) {
+        //     StdOut.println("main " + lineSegments);
+        // }
+
     }
 }
